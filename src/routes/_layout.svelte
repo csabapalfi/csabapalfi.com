103808c58c735cb6f73a3337ffbe7faa07d286bd
Csaba Palfi <csaba@palfi.me>
2019-05-12 14:28:12 +0100

    add typography.js

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,16 +1,27 @@
 <script context="module">
 	import embed_styles from '../utils/_embed_styles.js';
+	import Typography from 'typography'
+
+	const typography = new Typography({
+		baseFontSize: '18px',
+		baseLineHeight: 1.666,
+	});
 
 	export function preload({ params, query }) {
 		return this.fetch(`global.css`).then(r => r.text())
-			.then(styles => ({ globalStyles: embed_styles(styles)}));
+			.then(styles => ({
+				typographyStyles: embed_styles(typography.toString()),
+				globalStyles: embed_styles(styles),
+			}));
 	}
 </script>
 
 <script>
 	export let globalStyles;
+	export let typographyStyles;
 </script>
 
+{@html typographyStyles}
 {@html globalStyles}
 
 <style>
