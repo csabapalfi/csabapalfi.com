6dc143a8372ee0f6b2d5e33d9b73ec3f764130b1
Csaba Palfi <csaba@palfi.me>
2019-05-15 20:47:30 +0100

    fix? rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -3,9 +3,9 @@ import replace from 'rollup-plugin-replace';
 import commonjs from 'rollup-plugin-commonjs';
 import svelte from 'rollup-plugin-svelte';
 import babel from 'rollup-plugin-babel';
+import json from 'rollup-plugin-json';
 import { terser } from 'rollup-plugin-terser';
 import config from 'sapper/config/rollup.js';
-import pkg from './package.json';
 
 const mode = process.env.NODE_ENV;
 const dev = mode === 'development';
@@ -20,6 +20,7 @@ export default {
 				'process.browser': true,
 				'process.env.NODE_ENV': JSON.stringify(mode)
 			}),
+			json(),
 			svelte({
 				dev,
 				hydratable: true
@@ -65,7 +66,7 @@ export default {
 			resolve(),
 			commonjs()
 		],
-		external: Object.keys(pkg.dependencies).concat(
+		external: [].concat(
 			require('module').builtinModules || Object.keys(process.binding('natives'))
 		),
 	},
