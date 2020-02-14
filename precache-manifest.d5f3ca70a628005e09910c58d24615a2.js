ba09be90db1868caa5e8269ad9f80c3d71f0830c
Csaba Palfi <csaba@palfi.me>
2020-02-14 00:35:52 +0000

    Updates

---
+++ precache-manifest.d5f3ca70a628005e09910c58d24615a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6b9ecdb6bfdda6a0ca1ffb8e0cc4011",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea12bb8758a553740e2c",
+    "url": "/static/js/2.be1d89d1.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.be1d89d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b9a1e6710c94aba6c2a",
+    "url": "/static/js/main.052c78e4.chunk.js"
+  },
+  {
+    "revision": "ee29d5653a2ae1a31e65",
+    "url": "/static/js/runtime-main.016fcb26.js"
+  },
+  {
+    "revision": "c994f76f48c0329887eded1a8d28573c",
+    "url": "/static/media/csaba192.c994f76f.jpg"
+  },
+  {
+    "revision": "d275938dacac5d711581bf8765c00baa",
+    "url": "/static/media/csaba384.d275938d.jpg"
+  }
+]);
