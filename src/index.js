e040e7c97d650b1c705305655277f67181e109e4
Csaba Palfi <csaba@palfi.me>
2021-04-27 21:45:29 +0100

    simplify

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-const rootElement = document.getElementById('root');
-if (!rootElement.hasChildNodes()) {
-  ReactDOM.render(<App />, rootElement);
-}
